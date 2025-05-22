ee7e1f2a489b1dc04f3850908ca2caaf8db50d82
DLillard12 <dannylillard12@gmail.com>
2025-05-22 13:09:34 -0500

    pixelation works now.

--- b_w_pixilator.py
+++ b_w_pixilator.py
@@ -3,45 +3,54 @@
 # This is a small project to pixilate an image and turn in b & w
 # inspired by: https://www.youtube.com/watch?v=nvR8__cVifI
 
+# ---------------
+#   Imports
+# ---------------
 import numpy as np
 from PIL import Image
 import random
 
-img = Image.open("input\\woman.jpg")
+# --------------
+# Functions
+# --------------
+
+def pixelate(pixelation_factor: int , img: Image):
+    tiny = img.resize(size=[int(img.width/pixelation_factor),int(img.height/pixelation_factor)])
+    return tiny
+
+
+def binary_threshold(img: Image):
+    pixels = img.load()  # Gives access to pixel data
+    for x in range(img.width):
+        for y in range(img.height):
+            if pixels[x, y] > 70: # for now just setting threshold to halfway.
+                pixels[x,y] = 255
+            else:
+                pixels[x,y] = 0
+    return img
+
+def add_random_pixels(img: Image, random_factor: int):
+    width, height = img.size
+    
+    pixels = img.load()  # Gives access to pixel data
+    num_rand_pixels = random.randint(0,arr.shape[0] * arr.shape[1] // random_factor)
+    for _ in range(num_rand_pixels):
+        rand_x = random.randint(0,width-1)
+        rand_y = random.randint(0,height-1)
+        pixels[rand_x, rand_y] = random.randint(0,255)
+    print('Number of random pixels: ', num_rand_pixels)
+    return img
+
+img = Image.open("input\\tonysoprano.jpg")
 img = img.convert(mode='L') # converting to greyscale
 
 # converting to a numpy array so we can set the pixel color.
 arr = np.array(img)
-print(arr.shape)  # (784, 1200, 3) or (784, 1200) for grayscale
-print(arr[0])
 
-# setting the random elements.
-num_rand_pixels = random.randint(0,int(arr.shape[0] * arr.shape[1] / 4))
-print('Number of random pixels: ', num_rand_pixels)
-
-rand_pixel_matrix = []
-# randomly choosing where those random pixels should go:
-for i in range(num_rand_pixels):
-    rand_x = random.randint(0,arr.shape[0]-1)
-    rand_y = random.randint(0,arr.shape[1]-1)
-    arr[rand_x][rand_y] = random.randint(0,255)
+# Performing operations
+pixelated = pixelate(12,img)  # Higher is more pixelated
+pixelated_random = add_random_pixels(pixelated,1024) # lower is more random pixels
+pixelated_binary_random = binary_threshold(pixelated_random)
 
 
-
-for x in range(arr.shape[0]):
-    for y in range(arr.shape[1]):
-        if arr[x][y] > 127: # for now just setting threshold to halfway.
-            arr[x][y] = 255
-        else:
-            arr[x][y] = 0
-
-img = Image.fromarray(arr)
-
-# pixelating, first trying to pixelate after the floor/ceiling function.
-
-resize_factor = 16
-tiny = img.resize(size=[int(arr.shape[1]/resize_factor),int(arr.shape[0]/resize_factor)])
-
-pixelated = tiny.resize(img.size,Image.BOX)   # resizing the smaller image to the original size
-
-pixelated.save(fp='output\\pixelated_rand_grey_woman.jpg')
+pixelated_binary_random.save(fp='output\\tonysoprano.png')
--- readme.md
+++ readme.md
@@ -13,3 +13,20 @@ So it appears that the video had some random factor that was adding in pixels th
 I will be adding this stochastic element.
 
 I was having issues with pixelation, because I was using the wrong variable name! Took me forever to find.
+
+I am also using this to play around with branches for the first time.
+
+I have noticed that the pixelation is grey, this is because I am setting the pixels at a floor/ceiling BEFORE pixelization.
+
+I tried 127 for the binary threshold but that is a bit too high, setting it lower.
+
+Also I have noticed that while the video has binary white and black the pixels have a gradient, so there are grouping of pixels which are all black, all white, and inbetween there are groupings with a black or white X.
+E.G.
+BBB
+BBB
+BBB
+---
+BWB
+WBW
+BWB
+---
